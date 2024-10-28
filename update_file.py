23573ea25348b0599270c9168093a0c2f59452a5
asingh <amansingh427@icloud.com>
2024-10-28 14:13:22 -0400

    added cv

--- update_file.py
+++ update_file.py
@@ -6,10 +6,13 @@ with open("config.json") as f:
     file_paths = json.load(f)['file_paths']
 
 resume_path = "resume.pdf"
+cv_path = "detailed_cv.pdf"
 
-file_name = "aman_singh_resume.pdf"
+resume_name = "aman_singh_resume.pdf"
+cv_name = "aman_singh_cv.pdf"
 
 if __name__ == "__main__":
     """ Using this script to copy resume to other parts of my computer when I push changed to resume, using git hooks """
     for fp in file_paths:
-        shutil.copyfile(resume_path, os.path.join(fp, file_name))
+        shutil.copyfile(resume_path, os.path.join(fp, resume_name))
+        shutil.copyfile(cv_path, os.path.join(fp, cv_name))
